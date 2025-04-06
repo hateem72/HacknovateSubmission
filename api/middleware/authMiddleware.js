ba0cdee9c059ed2f7ca97acbf5566b883cd9a8ad
hateem72 <146501209+hateem72@users.noreply.github.com>
2025-04-06 16:43:17 +0530

    Delete api directory

--- api/middleware/authMiddleware.js
+++
@@ -1,7 +0,0 @@
-const authMiddleware = (req, res, next) => {
-    if (!req.session.user) return res.status(401).json({ message: "Not authenticated" });
-    req.user = req.session.user; 
-    next();
-  };
-  
-  export default authMiddleware;
